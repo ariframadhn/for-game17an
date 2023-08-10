f320f1a87e84b63c64a30891923c16b65a63c7b6
ariframadhn <arifboys234@gmail.com>
2023-08-10 08:16:04 +0700

    menambahkan asset baru lagi

--- js/game.js
+++ js/game.js
@@ -12,7 +12,7 @@ var gambar = {
   idle: "Idle (32x32) new 7.png",
   run: "Run (32x32) new 2.png",
   jump: "Jump (32x32) new 1.png",
-  fall: "Fall (32x32).png",
+  fall: "Fall (32x32) new 1.png",
   hit: "Hit (32x32).png",
   tileset: "Terrain (512x512).png",
   bg: "Blue.png",
